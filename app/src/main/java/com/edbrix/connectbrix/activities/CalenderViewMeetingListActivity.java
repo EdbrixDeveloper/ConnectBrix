9a41922c256fa343bb26a2eb4807f9c9c2b588f0
Vinayak <VinayakP@edbrix.com>
2019-06-11 17:59:28 +0530

    user group icon  by pranav 11052019

--- app/src/main/java/com/edbrix/connectbrix/activities/CalenderViewMeetingListActivity.java
+++ app/src/main/java/com/edbrix/connectbrix/activities/CalenderViewMeetingListActivity.java
@@ -1,39 +1,44 @@
 package com.edbrix.connectbrix.activities;
 
 import android.content.Intent;
-import android.support.annotation.NonNull;
+import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.CalendarView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.applandeo.materialcalendarview.CalendarView;
+import com.applandeo.materialcalendarview.EventDay;
+import com.applandeo.materialcalendarview.listeners.OnDayClickListener;
 import com.edbrix.connectbrix.R;
 import com.edbrix.connectbrix.adapters.SchoolListAdapter;
+import com.edbrix.connectbrix.data.MyEventDay;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 
 public class CalenderViewMeetingListActivity extends AppCompatActivity {
 
     private LinearLayout mLinearLayoutCal;
     private CalendarView mCalendarView;
+
     private ListView mMeetingListWithCalender;
     private TextView mTxtSelectedDate;
     private FloatingActionButton mFloatingActionButtonFabWithListview;
     FloatingActionButton floating_action_button_fab_with_listview;
     SchoolListAdapter schoolListAdapter;
     ArrayList<String> date = new ArrayList<>();
-
+    private List<EventDay> mEventDays = new ArrayList<>();
     String str_temp_date;
 
 
@@ -58,8 +63,8 @@ public class CalenderViewMeetingListActivity extends AppCompatActivity {
         mMeetingListWithCalender.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent intent = new Intent(CalenderViewMeetingListActivity.this,MeetingDetailsActivity.class);
-                intent.putExtra("meetingTitle","School Anual Function Meeting");
+                Intent intent = new Intent(CalenderViewMeetingListActivity.this, MeetingDetailsActivity.class);
+                intent.putExtra("meetingTitle", "School Anual Function Meeting");
                 startActivity(intent);
             }
         });
@@ -74,7 +79,7 @@ public class CalenderViewMeetingListActivity extends AppCompatActivity {
         Date date = new Date();
         mTxtSelectedDate.setText(finalSimpleDateFormat.format(date));
 
-        mCalendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+        /*mCalendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
             public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
                 SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
@@ -88,8 +93,49 @@ public class CalenderViewMeetingListActivity extends AppCompatActivity {
                     e.printStackTrace();
                 }
             }
+        });*/
+
+        mCalendarView.setOnDayClickListener(new OnDayClickListener() {
+            @Override
+            public void onDayClick(EventDay eventDay) {
+                try {
+                    MyEventDay myEventDay = (MyEventDay) eventDay;
+                    Calendar cal = myEventDay.getCalendar();
+                    Date date = cal.getTime();
+                    mTxtSelectedDate.setText(finalSimpleDateFormat.format(date));
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    Calendar cal1 = mCalendarView.getSelectedDate();
+                    Date date = cal1.getTime();
+                    mTxtSelectedDate.setText(finalSimpleDateFormat.format(date));
+
+                }
+            }
         });
 
+        addEventToCalendar();
+
+    }
+
+    private void addEventToCalendar() {
+
+        try {
+
+            for(int i=1;i<=9;i++){
+                SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+                Date dateForEvent = sdf. parse(i+"-06-2019");
+                Calendar cal = Calendar. getInstance();
+                cal.setTime(dateForEvent);
+                MyEventDay myEventDay1 = new MyEventDay(cal, R.drawable.ic_fiber_manual_record_black_24dp, "Note "+i);
+                mCalendarView.setDate(myEventDay1.getCalendar());
+                mEventDays.add(myEventDay1);
+                mCalendarView.setEvents(mEventDays);
+            }
+
+
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
     }
 
     private void assignViews() {
@@ -97,11 +143,11 @@ public class CalenderViewMeetingListActivity extends AppCompatActivity {
         mCalendarView = (CalendarView) findViewById(R.id.calendarView);
         mMeetingListWithCalender = (ListView) findViewById(R.id.meetingListWithCalender);
         mFloatingActionButtonFabWithListview = (FloatingActionButton) findViewById(R.id.float_btn);
-        mTxtSelectedDate = (TextView)findViewById(R.id.txtSelectedDate);
+        mTxtSelectedDate = (TextView) findViewById(R.id.txtSelectedDate);
     }
 
     private void setSchoolListAdapter(ArrayList<String> date) {
-        schoolListAdapter = new SchoolListAdapter(CalenderViewMeetingListActivity.this,date);
+        schoolListAdapter = new SchoolListAdapter(CalenderViewMeetingListActivity.this, date);
         mMeetingListWithCalender.setAdapter(schoolListAdapter);
     }
 
